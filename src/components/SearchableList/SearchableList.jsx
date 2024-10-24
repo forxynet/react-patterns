e8152b14dec78029a5267b583a78f7d601786752
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-24 20:45:42 +0300

    upsate search

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 
-export default function SearchableList({ items, children }) {
-  const [searchTerm, setSearchTerm] = useState('');
+export default function SearchableList({ items, itemKeyFn, children }) {
+    const lastChange = useRef();
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const searchResults = items.filter((item) =>
-    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    const searchResults = items.filter((item) =>
+        JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-  function handleChange(event) {
-    setSearchTerm(event.target.value);
-  }
+    function handleChange(event) {
+        if (lastChange.current) {
+            clearTimeout(lastChange.current);
+        }
 
-  return (
-    <div className="searchable-list">
-      <input type="search" placeholder="Search" onChange={handleChange} />
-      <ul>
-        {searchResults.map((item, index) => (
-            <li key={index}>
-                {children(item)}
-            </li>
-        ))}
-      </ul>
-    </div>
-  );
+        lastChange.current = setTimeout(() => {
+            setSearchTerm(event.target.value);
+        }, 500);
+    }
+
+    return (
+        <div className="searchable-list">
+            <input type="search" placeholder="Search" onChange={handleChange} />
+            <ul>
+                {searchResults.map((item) => (
+                    <li key={itemKeyFn(item)}>
+                        {children(item)}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
