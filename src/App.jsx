2a79de282e704e68eef6d1d18601614d6c980175
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-24 20:29:34 +0300

    update app with Place for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
 import Accordion from './components/Accordion/Accordion.jsx';
+import SearchableList from './components/SearchableList/SearchableList.jsx';
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+import Place from './Place.jsx';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return (
@@ -7,14 +47,11 @@ function App() {
         <h2>Why work with us?</h2>
 
         <Accordion className="accordion">
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title className="accordion-item-title" id="experience">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We got 20 years of experience
             </Accordion.Title>
-            <Accordion.Content
-              className="accordion-item-content"
-              id="experience"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>You can&apos;t go wrong with us.</p>
                 <p>
@@ -24,14 +61,11 @@ function App() {
               </article>
             </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title id="local-guides" className="accordion-item-title">
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We are working with local guides
             </Accordion.Title>
-            <Accordion.Content
-              id="local-guides"
-              className="accordion-item-content"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>We are not doing this along from our office.</p>
                 <p>
@@ -42,6 +76,14 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
         </Accordion>
+      </section>
+      <section>
+        <SearchableList items={PLACES}>
+          {(item) => <Place item={item}  />}
+        </SearchableList>
+        <SearchableList items={['item 1', 'item 2']} >
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
--- src/components/Accordion/AccordionTitle.jsx
+++ src/components/Accordion/AccordionTitle.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useAccordionContext } from './Accordion'
+import { useAccordionItemContext } from './AccordionItem';
 
-export default function AccordionTitle({id, className, children}) {
-  const {toggleItem} = useAccordionContext();
+export default function AccordionTitle({ className, children}) {
+    const { toggleItem } = useAccordionContext();
+    const id = useAccordionItemContext();
+    
     return (
         <h3 className={className} onClick={() => toggleItem(id)}>
             {children}
